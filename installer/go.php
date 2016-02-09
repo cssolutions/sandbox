88f3d4450d831bb1d174990ccfefea1830698352
cssolutions <f.csaba@vipmail.hu>
2016-02-09 16:20:42 +0100

    installer

---
+++ installer/go.php
@@ -0,0 +1,36 @@
+#!/usr/bin/env php
+<?php
+
+$longopts  = array(
+    "name:",     // Required value
+);
+
+$options = getopt($shortopts, $longopts);
+extract($options);
+if(!isset($name)) die('Name must be provided!')
+
+//UPDATE HOST AND VIRTUAL HOST FILES
+
+
+
+
+
+//CREATE/CLONE PROJECT
+
+
+
+
+
+
+
+
+//INSTALL DEPENDENCIES
+
+
+
+
+
+
+
+
+//INSTALL FRONTEND DEPENDENCIES
