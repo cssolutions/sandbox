783326c9398201deed36860b1566f9fac58ceaf4
cssolutions <f.csaba@vipmail.hu>
2016-02-10 09:38:39 +0100

    installer

--- installer/go.php
+++ installer/go.php
@@ -1,11 +1,12 @@
-#!/usr/bin/env php
+#!/opt/lampp/php
 <?php
 
 define('HOST_PATH','/home/csaba/test/hosts');
 define('VHOST_PATH','/home/csaba/test/vhosts');
 define('WWW_PATH','/home/csaba/www');
+define('INSTALLER_PATH', realpath('.'));
 
-
+$cur_dir = getcwd();
 $shortopts = "";
 $longopts  = [
     "name:",     // Required value
@@ -17,14 +18,12 @@ $url = "$name.local";
 
 if(!isset($name)) die('Name must be provided!');
 
-//print $url;
-
 //UPDATE HOST AND VIRTUAL HOST FILES
 if(strstr(file_get_contents(HOST_PATH), $url)) {
 	echo "Már létezik a $url a következőben: " . HOST_PATH . "\n";
 }
 else {
-	shell_exec("echo '127.0.0.1	$url' >> " . HOST_PATH);
+	passthru("echo '127.0.0.1	$url' >> " . HOST_PATH);
 	echo "A(z) $url létrehozva a következőben: " . HOST_PATH . "\n";
 }
 
@@ -45,7 +44,7 @@ if(strstr(file_get_contents(VHOST_PATH), $url)) {
 	echo "Már létezik a $url a következőben: " . VHOST_PATH . "\n";
 }
 else {
-	shell_exec("echo '$content' >> ".VHOST_PATH);
+	passthru("echo '$content' >> ".VHOST_PATH);
 	echo "A(z) $url létrehozva a következőben: " . VHOST_PATH . "\n";
 }
 
@@ -55,12 +54,13 @@ else {
 
 
 
-//IMPORT PROJECT
-
-
-
 //INSTALL DEPENDENCIES
-
+chdir(WWW_PATH);
+if(!file_exists($name)) mkdir($name);
+chdir($name);
+passthru("cp -rf " . INSTALLER_PATH . "/project/*.* $www_dir");
+passthru("curl -sS https://getcomposer.org/installer | php");
+passthru("php composer.phar install");
 
 
 
