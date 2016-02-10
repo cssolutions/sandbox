12c8216fda6264ffdce02e788705d11dd0475b04
cssolutions <f.csaba@vipmail.hu>
2016-02-10 10:37:06 +0100

    installer

--- installer/go.php
+++ installer/go.php
@@ -11,11 +11,27 @@ define('HOST_PATH','/home/csaba/test/hosts');
 define('VHOST_PATH','/home/csaba/test/vhosts');
 define('WWW_PATH','/home/csaba/www');
 define('INSTALLER_PATH', realpath('.'));
+$bower_config = [
+	"name" => "project",
+	"version" => "0.1.0",
+	"license" => "MIT",
+	"authors" => ["Csaba Farkas"],
+	"dependencies" => [
+	    "bootstrap"=> "~3.3.5",
+	    "bootstrap-sass-official"=> "~3.3.5",
+	    "datatables"=> "DataTables#~1.10.8",
+	    "font-awesome"=> "~4.4.0",
+	    "fullcalendar"=> "~2.4.0",
+	    "jquery"=> "~2.1.4",
+	    "jquery-ui"=> "~1.11.4",
+	    "summernote"=> "~0.6.16"
+	]
+];
 
 $cur_dir = getcwd();
 $shortopts = "";
 $longopts  = [
-    "name:",     // Required value
+    "name:"     // Required value
 ];
 
 $options = getopt($shortopts, $longopts);
@@ -25,9 +41,7 @@ $url = "$name.local";
 if(!isset($name)) die('Name must be provided!');
 
 //UPDATE HOST AND VIRTUAL HOST FILES
-if(strstr(file_get_contents(HOST_PATH), $url)) {
-	echo "Már létezik a $url a következőben: " . HOST_PATH . "\n";
-}
+if(strstr(file_get_contents(HOST_PATH), $url)) echo "Már létezik a $url a következőben: " . HOST_PATH . "\n";
 else {
 	passthru("echo '127.0.0.1	$url' >> " . HOST_PATH);
 	echo "A(z) $url létrehozva a következőben: " . HOST_PATH . "\n";
@@ -46,28 +60,28 @@ $content = <<<EOT
 </VirtualHost>		  
 EOT;
 
-if(strstr(file_get_contents(VHOST_PATH), $url)) {
-	echo "Már létezik a $url a következőben: " . VHOST_PATH . "\n";
-}
+if(strstr(file_get_contents(VHOST_PATH), $url)) echo "Már létezik a $url a következőben: " . VHOST_PATH . "\n";
 else {
 	passthru("echo '$content' >> ".VHOST_PATH);
 	echo "A(z) $url létrehozva a következőben: " . VHOST_PATH . "\n";
 }
 
-
 //CREATE/CLONE PROJECT
-
 chdir(WWW_PATH);
 passthru('git clone https://oninflo-csaba@bitbucket.org/oninflo-csaba/base-admin.git');
 passthru("mv base-admin $name");
 chdir($name);
 passthru('rm -rf repo.git');
 passthru('git init');
-passthru("git remote add origin https://oninflo-csaba@bitbucket.org/oninflo-csaba/$name.git")
+//passthru("git remote add origin https://oninflo-csaba@bitbucket.org/oninflo-csaba/$name.git")
 
+//INSTALL FRONTEND DEPENDENCIES
+passthru("cp -rf " . INSTALLER_PATH . "/project/*.* $www_dir");
+passthru("cp -rf " . INSTALLER_PATH . "/project/.bowerrc $www_dir");
+file_put_contents($www_dir . '/bower.json', json_encode($bower_config, true));
 
 //INSTALL DEPENDENCIES
-passthru("cp -rf " . INSTALLER_PATH . "/project/*.* $www_dir");
+passthru('bower update');
 passthru("curl -sS https://getcomposer.org/installer | php");
 passthru("php composer.phar install");
 
@@ -77,4 +91,6 @@ passthru("php composer.phar install");
 
 
 
-//INSTALL FRONTEND DEPENDENCIES
+
+
+
