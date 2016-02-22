7af45f41d7f6f2f9fe8a61115ed24d6023f5d7d0
cssolutions <f.csaba@vipmail.hu>
2016-02-22 15:37:35 +0100

    others

--- installer-mvc/config.php
+++ installer-mvc/config.php
@@ -1,6 +1,18 @@
 <?php
 
+//create a default Validator, that use $config[mvc][név]
+
+
 $config = [
-	'name'    => '[name]',
+	'name'    => '[name]',	//name of the application and database too
+	'mvc'     => ['[name]' => [
+										'sql'  => '[query]',
+										'attributes' => [
+																'[field_name1]' => 'validators separated by |',
+																'[field_name2]' => 'validators separated by |',
+																'[field_name3]' => 'validators separated by |',
+															 ]
+		                       ]
+					 ],
 	'skipped' => [],
 ];
