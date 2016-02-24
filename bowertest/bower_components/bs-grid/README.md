aa222a447cf541530ddd5b4c9f8c821ac4535d70
cssolutions <f.csaba@vipmail.hu>
2016-02-24 16:29:01 +0100

    dev

---
+++ bowertest/bower_components/bs-grid/README.md
@@ -0,0 +1,22 @@
+## bs-grid
+bs-grid takes the Bootstrap Grid, and puts it into your project for a light-weight grid system without all of the hassle of Bootstrap!
+
+
+## Usage
+Use `bower install bs-grid` to download the project.
+
+
+If you want `less` files, go to `src/less/assets/stylesheets`.
+
+If you want `scss` files, go to `src/scss/assets/stylesheets`.
+
+If you would like just the `css` file, go to `dist/assets/stylesheets/bs-grid.css`.
+
+## TODO:
+Add `less` files.
+
+Setup `gulpfile.js` to compile `less` files.
+
+Test with more demos.
+
+Work on <a href="http://bin0.github.io/bs-grid">bin0.github.io/bs-grid</a>
--- installer/go.php
+++ installer/go.php
@@ -1,6 +1,20 @@
 #!/opt/lampp/php
 <?php
+//create a project at bitbucket with the name
 //RUN this script with: sudo php hosts.php --name=csaba;php go.php --name=csaba
+//RUN this script with: sudo php hosts.php --name=csaba -public=1;php go.php --name=csaba -public=1
+//RUN MVC-INSTALLER
+//http://bootsnipp.com/snippets/featured/responsive-sidebar-menu    implement this menu
+//
+//http://bootstrapious.com/free-templates 
+//http://www.gettemplate.com/
+//http://www.bootstrap51.com/
+//http://www.marcellop.com/bootmetro/
+//http://www.codeply.com/render/KrUO8QpyXP#
+//http://www.bootply.com/zrATdhAKBZ    parallax one page template
+//http://www.bootply.com/zylyqLRmcj     parallax
+//http://cardeostrap.cardeo.ca/   conatining all elements
+//http://bootsnipp.com/forms?version=3   for creating bootstrap forms
 
 
 //#!/usr/bin/env php
@@ -30,13 +44,17 @@ $bower_config = [
 	"license" => "MIT",
 	"authors" => ["Csaba Farkas"],
 	"dependencies" => [
-	    "bootstrap"=> "~3.3.5",
-	    "bootstrap-sass-official"=> "~3.3.5",
+	    "jquery"=> "~2.1.4",
+	    "jquery-ui"=> "~1.11.4",
+		 "jquery-popup-overlay"=> "~1.6.0",
+	    "bootstrap"=> "~3.3.6",
+	    "bootstrap-sass-official"=> "~3.3.6",
+		 "bootstrap-material-design"=> "0.2.1",
+		 "bootstrap-combobox"=> "1.1.6",
+		 "bs-confirmation"=> "~1.0.5",
 	    "datatables"=> "DataTables#~1.10.8",
 	    "font-awesome"=> "~4.4.0",
 	    "fullcalendar"=> "~2.4.0",
-	    "jquery"=> "~2.1.4",
-	    "jquery-ui"=> "~1.11.4",
 	    "summernote"=> "~0.6.16"
 	]
 ];
@@ -82,11 +100,32 @@ passthru("cp -rf " . INSTALLER_PATH . "/project/*.* $www_dir");
 passthru("cp -rf " . INSTALLER_PATH . "/project/.bowerrc $www_dir");
 file_put_contents($www_dir . '/bower.json', json_encode($bower_config, true));
 passthru('bower update');
-passthru('bower install bootstrap-material-design');
+passthru('bower install eonasdan-bootstrap-datetimepicker#latest --save');
+passthru('bower install bootstrap-star-rating');
+passthru('bower install --save datatables.net');
+passthru('bower install metisMenu');
+passthru('bower install bootstrap-treeview');
+passthru('bower install bootstrap-social');
 passthru('bower install blueimp-file-upload-node');
 passthru('bower install Chart-js');
-passthru('bower install angular-material-design-light');
-passthru('bower install angular angular-datetimepicker-ByGiro --save');
+passthru('bower register bootstrap-floating-labels https://github.com/fauxparse/bootstrap-floating-labels -f');
+passthru('bower register jq-range-slider https://github.com/ghusse/jQRangeSlider -f');
+passthru('bower register bs-grid https://github.com/pontikis/bs_grid -f');
+passthru('bower register bs-tabdrop https://github.com/jmschabdach/bootstrap-tabdrop -f');
+passthru('bower register bs-context-menu https://github.com/dgoguerra/bootstrap-menu -f');
+passthru('bower register bs-autohide-navbar https://github.com/istvan-ujjmeszaros/bootstrap-autohidingnavbar -f');
+passthru('bower register bs-bootbox https://github.com/makeusabrew/bootbox -f');
+passthru('bower install bootstrap-floating-labels');
+passthru('bower install jq-range-slider');
+passthru('bower install bs-grid');
+passthru('bower install bs-tabdrop');
+passthru('bower install bs-context-menu');
+passthru('bower install bs-autohide-navbar');
+passthru('bower install bs-bootbox');
+
+
+//passthru('bower install angular-material-design-light');
+//passthru('bower install angular angular-datetimepicker-ByGiro --save');
 print "---------------------------------------------------- Bower tasks has been ended.\n";
 
 //GULP TASKS
