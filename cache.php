4f81dc45861914533ddc797784f5fcaf9d2a18ab
cssolutions <f.csaba@vipmail.hu>
2016-03-11 15:36:43 +0100

    dev

--- cache.php
+++ cache.php
@@ -1,10 +1,10 @@
 <?php
 function removeCache($dir) {
-	$files = array_diff(scandir($dir),['.','..']);
-	foreach ($files as $file) {
+	foreach (array_diff(scandir($dir),['.','..']) as $file) {
 		if (strpos($file, 'session::') === 0 || strpos($file, 'debug::debug_info') === 0) continue;
 		unlink($dir . '/' . $file);
 	}	
 }
+
 removeCache('/mnt/develop/csaba.farkas/objfw/cache/develop.digicable.hu');
 removeCache('/mnt/develop/csaba.farkas/objfw/cache/tpl/compiled');
