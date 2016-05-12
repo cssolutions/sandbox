40c2a4bac71a4b1ac4f0ba39636292447e130b65
cssolutions <f.csaba@vipmail.hu>
2016-05-12 09:03:21 +0200

    dev

--- drpl/modules/teszt/templates/teszt.tpl.php
+++ drpl/modules/teszt/templates/teszt.tpl.php
@@ -2,13 +2,12 @@
 		<div class="form-group">
 			<label class="control-label col-xs-12 col-md-2" for="search"><?php print t('Search') ?>: </label>
 			<div class="col-xs-12 col-md-6">
-				<input id="search" type="text" class="form-control" size="60" autocomplete="off" placeholder="Írja be a keresett televízió márkáját és/vagy tipusát..." data-provide="typeahead" data-items="20" data-source="">
+				<input id="search" type="text" class="form-control" size="60" autocomplete="off" placeholder="Egy random string a placeholderhez!" data-provide="typeahead" data-items="20" data-source="">
 				<p>
-					<div class="alert alert-success hidden">A kiválasztott készülék támogatja a CI+ technológiát.</div>
+					<div class="alert alert-success hidden">Egy random string!</div>
 				</p>
 			</div>
 		</div>
 	</form><?php 
 	drupal_add_js(drupal_get_path('module', 'teszt').'/js/site.js');
-	drupal_add_css(drupal_get_path('module', 'digi_speedtest').'/css/ookla-netgauge.css'); 
 	?>
