4025cf38c0f8837b0c2186341486b66e5b81d135
cssolutions <f.csaba@vipmail.hu>
2016-09-16 16:34:06 +0200

    sockets

---
+++ php/socket/examples/notification/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8' />
+
+</head>
+<body>	
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+<script language="javascript" type="text/javascript">  
+$(document).ready(function(){
+	//create a new WebSocket object.
+	var wsUri = "ws://sandbox.local:8000/php/socket/brilliant_example/server.php"; 	
+	websocket = new WebSocket(wsUri); 
+	
+	websocket.onopen = function(ev) { // connection is open 
+		$('#message_box').append("<div class=\"system_msg\">Connected!</div>"); //notify user
+	}
+
+	//#### Message received from server?
+	websocket.onmessage = function(ev) {
+		var msg = JSON.parse(ev.data); //PHP sends Json data
+		var type = msg.type; //message type
+		var umsg = msg.message; //message text
+		var uname = msg.name; //user name
+		var ucolor = msg.color; //color
+
+		if(type == 'usermsg') $('#message_box').append("<div><span class=\"user_name\" style=\"color:#"+ucolor+"\">"+uname+"</span> : <span class=\"user_message\">"+umsg+"</span></div>");
+		if(type == 'system') $('#message_box').append("<div class=\"system_msg\">"+umsg+"</div>");
+		$('#message').val(''); //reset text
+	};
+	
+	websocket.onerror	= function(ev){$('#message_box').append("<div class=\"system_error\">Error Occurred - "+ev.data+"</div>");}; 
+	websocket.onclose 	= function(ev){$('#message_box').append("<div class=\"system_msg\">Connection Closed</div>");}; 
+});
+</script>
+<div class="chat_wrapper">
+<div class="message_box" id="message_box"></div>
+<div class="panel">
+<input type="text" name="name" id="name" placeholder="Your Name" maxlength="10" style="width:20%"  />
+<input type="text" name="message" id="message" placeholder="Message" maxlength="80" style="width:60%" />
+<button id="send-btn">Send</button>
+</div>
+</div>
+
+</body>
+</html>
